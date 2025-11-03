90610751ae93aad294eb40bbb705ff67832c5c6a
guptaDeepak <Deepakkumar346688@gmail.com>
2025-11-04 01:52:48 +0530

    updated and make code in structure

--- 1Speech_to text_convertor.js
+++
@@ -1,8 +0,0 @@
-function Speak(){
-	let text=document.getElementById("input").value;
-	let speak=new SpeechSynthesisUtterance(text)
-	speechSynthesis.speak(speak);
-} 
-function pause(){
-	speechSynthesis.pause();
-}
